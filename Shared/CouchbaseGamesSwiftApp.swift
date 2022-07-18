b829e9b0d624f388ff40e5ac4b8f659815100d21
Callum Birks <callum.birks@couchbase.com>
2022-07-18 12:38:25 +0100

    Initial Commit

---
+++ Shared/CouchbaseGamesSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  CouchbaseGamesSwiftApp.swift
+//  Shared
+//
+//  Created by Callum Birks on 18/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct CouchbaseGamesSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
